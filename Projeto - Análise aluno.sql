e6a76b0c2426aafb609cdde63eab40538633cf6a
Maik Rodrigues <69226200+MaikRodriguess@users.noreply.github.com>
2022-11-01 17:36:46 -0300

    Upgrade

--- Projeto - Análise aluno.sql
+++ Projeto - Análise aluno.sql
@@ -1,12 +1,16 @@
 -- Rascunho Tabelas
-select * from SHISTDISCOPTELETIVAS --(IDHANILITACAO, RA, PLETIVO,CODDISC, DISCIPLINA, STATUS DISCIPLINA, NOTA, FALTAS,IDTURMADISC)
-select * from SHISTDISCEQUIVCONCLUIDAS -- Possui mais linhas que a anterior(IDHANILITACAO, RA, PLETIVO,CODDISC, DISCIPLINA, STATUS DISCIPLINA,NUMCREDITOS, NOTA, FALTAS,IDTURMADISC)
-select distinct STATUS  from SHISTDISCCONCLUIDAS -- ()
-select distinct STATUS  from SHISTDISCPENDENTES 
-select * from SPARAM 
-select * from SMATRICULA --(IDTURMADISC, RA, IDPERLET, IDHABILITACAOFILIAL, TIPOMAT, TIPODISCIPLINA)
-select * from STURMADISC -- ()
-select * from v_Unasp_SHABILITACAOFILIALPL -- IDPERLET, IDHABILITACAOFILIAL
-select * from sstatus -- Status Da diciplina
 select * from ppessoa -- Nome 
-SELECT * FROM SPLETIVO 
+select * from SMATRICULA --(IDTURMADISC, RA, IDPERLET, IDHABILITACAOFILIAL, TIPOMAT, TIPODISCIPLINA)
+select * from PPESSOA --NOME, RA 
+SELECT * FROM SPLETIVO -- Descrição periodo letivo
+select * from SDISCIPLINA -- CODDISC, NOME
+select * from sstatus -- Status Da diciplina
+select * from gfilial -- Nome Filial, codfilial 
+select * from stipocurso -- Nome Nível de ensino
+select distinct TIPOMAT from smatricula -- Tipo matricula
+select * from scurso -- Nome curso 
+select * from SHABILITACAOALUNO -- Codigo Status do aluno no curso 
+select * from SNOTAETAPA -- Nota de disciplina
+select * from STURMADISC -- CODTURMA,CODDISC, IDPERLET,COLIGADA, CODFILIAL
+select * from v_Unasp_SHABILITACAOFILIALPL -- IDPERLET, IDHABILITACAOFILIAL
+
